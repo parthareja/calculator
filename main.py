8f7439d89f66e49505f74eca402156faf68bfabb
Aman Kaif <aman220202@gmail.com>
2023-02-27 20:07:55 +0530

    main driver completed

---
+++ main.py
@@ -0,0 +1,23 @@
+
+def __main__():
+    while True:
+        inp = int(input("""\n\nChoose Operation:    1. Addition    2. Subtraction    3. Multiplication    4. Division    5. Exit\n> """))
+
+        if inp == 5:
+            print("\nProgram exited.")
+            break
+
+        else:
+            a, b = [int(i) for i in input("Enter two numbers separated by spaces: ").split(" ")]
+
+            if inp == 1:
+                print(add(a, b))
+            elif inp == 2:
+                print(subtract(a, b))
+            elif inp == 3:
+                print(multiply(a, b))
+            elif inp == 4:
+                print(divide(a, b))
+        
+        
+__main__()
